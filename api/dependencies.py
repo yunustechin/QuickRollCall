3a25dcfaa1d98977069b196c5e2ba69b0fccbd74
yunustechin <yunustechin@gmail.com>
2025-06-20 19:55:56 +0300

    dependencies

--- api/dependencies.py
+++ api/dependencies.py
@@ -1,9 +1,22 @@
-from db import RedisClient
+from fastapi import Depends
 from functools import lru_cache
+from db import RedisClient
+from .services import SessionService
 
 @lru_cache(maxsize=1)
 def get_redis_client() -> RedisClient:
     """
-    Returns a singleton instance of the RedisClient.
+    Provides a singleton RedisClient instance for the application.
+
+    Uses @lru_cache to ensure a single, efficient connection is shared.
     """
     return RedisClient()
+
+def get_session_service(redis: RedisClient = Depends(get_redis_client)) -> SessionService: 
+    """
+    Dependency provider for the SessionService.
+
+    Initializes the service with required dependencies (like RedisClient)
+    for easy use in route handlers.
+    """
+    return SessionService(redis)
